de9482789086024a6bdc8168b915cc453905f5ff
Milos Dokic <milosdokic13@gmail.com>
2020-10-10 20:48:39 +1100

    changing status on checkboxes

--- src/App.js
+++ src/App.js
@@ -8,12 +8,27 @@ class App extends React.Component{
     this.state = {
       todos: todosData
     }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(id){
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      return {
+        todos: updatedTodos
+      }
+    })
   }
   render(){
-    const todos = this.state.todos.map(task => <ToDoItem item={task} />)
+    const todoItems = this.state.todos.map(task => <ToDoItem key={task.id} item={task} 
+    handleChange={this.handleChange} />)
     return(
       <div className="todo-list">
-      {todos}
+      {todoItems}
       </div>
     )
   }
