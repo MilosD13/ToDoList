a97e4d90969feeab106e1f60e8446d6f510241b3
Milos Dokic <milosdokic13@gmail.com>
2020-10-10 18:51:34 +1100

    making changes to style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import ToDoItem from "./ToDoItem"
+import todosData from "./TodosData"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      todos: todosData
+    }
+  }
+  render(){
+    const todos = this.state.todos.map(task => <ToDoItem item={task} />)
+    return(
+      <div className="todo-list">
+      {todos}
+      </div>
+    )
+  }
 }
 
 export default App;
